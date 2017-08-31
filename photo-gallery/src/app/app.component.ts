1c0805718da5134c12f4adf634d27e2a5c3c3147
Zabamund <fracgeol@gmail.com>
2017-08-31 17:11:23 +0200

    create new app photo-gallery

---
+++ photo-gallery/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pgy-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pgy';
+}
