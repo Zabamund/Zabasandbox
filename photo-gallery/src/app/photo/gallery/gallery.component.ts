c1bc86675085bb101334e9953f11e3786b42cbf5
Zabamund <fracgeol@gmail.com>
2017-09-01 09:11:51 +0200

    create gallery component

---
+++ photo-gallery/src/app/photo/gallery/gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pgy-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
