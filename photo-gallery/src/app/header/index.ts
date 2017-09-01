64070f1923983d295c06f1356667e692c9b9c692
Zabamund <fracgeol@gmail.com>
2017-09-01 10:03:04 +0200

    export all using index files

---
+++ photo-gallery/src/app/header/index.ts
@@ -0,0 +1,4 @@
+export * from './greeting/greeting.component';
+export * from './time/time.component';
+export * from './title/title.component';
+export * from './header.module';
---
+++ photo-gallery/src/app/photo/index.ts
@@ -0,0 +1,4 @@
+export * from './detail/detail.component';
+export * from './form/form.component';
+export * from './gallery/gallery.component';
+export * from './photo.module';
