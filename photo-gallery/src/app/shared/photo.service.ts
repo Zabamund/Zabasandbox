3a3bd9b77a40b9c65ef9b4dd44dbd413a3e07b74
Zabamund <fracgeol@gmail.com>
2017-09-01 10:46:41 +0200

    photo service imports and add service

--- photo-gallery/src/app/shared/photo.service.ts
+++ photo-gallery/src/app/shared/photo.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Photo } from '../photo';
 
 @Injectable()
 export class PhotoService {
 
-  constructor() { }
+  url = 'http://localhost:3000/photos';
+  private headers = new HttpHeaders ({ 'Content-Type': 'application/json' });
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  add(photo: Photo): Observable<Photo> {
+    return this.httpClient
+      .post(this.url, photo,
+      { headers: this.headers })
+      .map((response: Response) => response.json());
+  }
 
 }
