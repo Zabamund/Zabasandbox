b8f8c3daff3fe322ce34bfd0c938a9f37a8681d0
Zabamund <fracgeol@gmail.com>
2017-09-01 10:45:17 +0200

    correct imports to app.module

--- photo-gallery/src/app/app.module.ts
+++ photo-gallery/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +16,6 @@ import { PhotoService, TimeService } from './shared';
     BrowserModule,
     HeaderModule,
     PhotoModule,
-    HttpClient
   ],
   providers: [
     PhotoService,
