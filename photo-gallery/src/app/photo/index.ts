e3b2b78a51ceb7cecba0d042489d68072bbdc5b2
Zabamund <fracgeol@gmail.com>
2017-09-01 10:25:59 +0200

    added photo.model export

--- photo-gallery/src/app/photo/index.ts
+++ photo-gallery/src/app/photo/index.ts
@@ -2,3 +2,4 @@ export * from './detail/detail.component';
 export * from './form/form.component';
 export * from './gallery/gallery.component';
 export * from './photo.module';
+export * from './photo.model';
