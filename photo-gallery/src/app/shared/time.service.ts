6ed9899163537804ed39c912b0e08a64373a693f
Zabamund <fracgeol@gmail.com>
2017-09-01 11:02:42 +0200

    added time service

--- photo-gallery/src/app/shared/time.service.ts
+++ photo-gallery/src/app/shared/time.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/share';
 
 @Injectable()
 export class TimeService {
 
-  constructor() { }
+  private clock: Observable<Date>;
+
+  constructor() {
+    this.clock = Observable
+      .interval(1000)
+      .map(tick => new Date())
+      .share();
+  }
+
+  getClock(): Observable<Date> {
+    return this.clock;
+  }
 
 }
