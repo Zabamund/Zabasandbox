3e2c427d150b16d2b173edb616c239f7f14c3127
Zabamund <fracgeol@gmail.com>
2017-09-02 14:59:59 +0200

    add HttpClientModule to app.module.ts

--- photo-gallery/src/app/app.module.ts
+++ photo-gallery/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -16,6 +17,7 @@ import { PhotoService, TimeService } from './shared';
     BrowserModule,
     HeaderModule,
     PhotoModule,
+    HttpClientModule
   ],
   providers: [
     PhotoService,
