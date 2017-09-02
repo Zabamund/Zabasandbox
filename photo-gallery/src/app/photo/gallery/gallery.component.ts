e1f430ab1770d0b059fddcb298df7100e8f0b670
Zabamund <fracgeol@gmail.com>
2017-09-02 15:00:44 +0200

    add getAll photos to gallery component and display in template

--- photo-gallery/src/app/photo/gallery/gallery.component.ts
+++ photo-gallery/src/app/photo/gallery/gallery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PhotoService, Photo } from '../../shared';
+
 @Component({
   selector: 'pgy-gallery',
   templateUrl: './gallery.component.html',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  photos: Photo[] = [];
+  clickedPhoto: Photo;
+  photoToEdit: Photo;
+  showNewForm = false;
+
+  constructor(
+    private photoService: PhotoService
+  ) { }
 
   ngOnInit() {
+    this.loadPhotos();
+  }
+
+  loadPhotos() {
+    this.photoService
+      .getAll()
+      .subscribe((photos) => this.photos = photos);
   }
 
 }
