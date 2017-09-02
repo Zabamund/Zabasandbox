51af3222f95b0e5d97d2e1580e9ae1b2c7bee5f6
Zabamund <fracgeol@gmail.com>
2017-09-02 10:10:54 +0200

    removed dead code

--- photo-gallery/src/app/header/title/title.component.ts
+++ photo-gallery/src/app/header/title/title.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pgy-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class TitleComponent {}
