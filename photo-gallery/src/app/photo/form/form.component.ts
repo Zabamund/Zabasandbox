b82fbf3bd770b28363994ec2a43e23cc5acd1569
Zabamund <fracgeol@gmail.com>
2017-09-03 10:04:16 +0200

    form onSubmit function

--- photo-gallery/src/app/photo/form/form.component.ts
+++ photo-gallery/src/app/photo/form/form.component.ts
@@ -1,6 +1,8 @@
 import { Component, Output, Input } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
 import { PhotoService, Photo } from '../../shared';
 
 @Component({
@@ -19,12 +21,39 @@ export class FormComponent {
   @Input()
   photo = new Photo();
 
+  error: string;
+  isSaving = false;
+  saveOK = false;
+
   constructor(
     private photoService: PhotoService
   ) { }
 
   close() {
     this.onClose.emit(true);
+  }
+
+  onSubmit() {
+    this.isSaving = true;
+    let save: Observable<Photo>;
+    if (this.photo.id > 0) {
+      save = this.photoService.update(this.photo);
+    } else {
+      save = this.photoService.add(this.photo);
+    }
+    save.subscribe(
+      (inputPhoto) => {
+        this.photo = inputPhoto;
+        this.saveOK = true;
+        setTimeout(() => this.isSaving = false, 500);
+        setTimeout(() => this.saveOK = false, 2000);
+        this.onSave.emit(true);
+      },
+      (error) => {
+        this.error = error;
+        this.isSaving = false;
+      }
+    );
   }
 
 }
