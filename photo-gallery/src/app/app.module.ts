714efb891f3ee68e6d224332513f06e287459b1d
Zabamund <fracgeol@gmail.com>
2017-09-03 10:17:45 +0200

    removed not needed formsmodule from app.module- formsmodule imported at using module level, not root

--- photo-gallery/src/app/app.module.ts
+++ photo-gallery/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -18,8 +17,7 @@ import { PhotoService, TimeService } from './shared';
     BrowserModule,
     HeaderModule,
     PhotoModule,
-    HttpClientModule,
-    FormsModule
+    HttpClientModule
   ],
   providers: [
     PhotoService,
