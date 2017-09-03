71af3c7e4d5ce9bf997eb23608237cd40b65a552
Zabamund <fracgeol@gmail.com>
2017-09-03 10:16:57 +0200

    added FormsModule to photo.module

--- photo-gallery/src/app/photo/photo.module.ts
+++ photo-gallery/src/app/photo/photo.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { GalleryComponent } from './gallery/gallery.component';
 import { DetailComponent } from './detail/detail.component';
 import { FormComponent } from './form/form.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [
     GalleryComponent,
