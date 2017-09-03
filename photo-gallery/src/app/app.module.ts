bd8c7b9862cf68255f3078a9ced5ba30c53b668c
Zabamund <fracgeol@gmail.com>
2017-09-03 10:09:08 +0200

    added FormsModule to app.module

--- photo-gallery/src/app/app.module.ts
+++ photo-gallery/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +18,8 @@ import { PhotoService, TimeService } from './shared';
     BrowserModule,
     HeaderModule,
     PhotoModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     PhotoService,
