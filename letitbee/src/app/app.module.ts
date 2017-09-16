cfd7926ba2bafe42fc3cfe9596f68428a18412f7
Zabamund <fracgeol@gmail.com>
2017-09-16 12:24:28 +0200

    import icons module

--- letitbee/src/app/app.module.ts
+++ letitbee/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MdToolbarModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MdToolbarModule } from '@angular/material';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdToolbarModule
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
