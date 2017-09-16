1522607e9ac1f0027f28152d92a29cfb082c0fbc
Zabamund <fracgeol@gmail.com>
2017-09-16 12:02:51 +0200

    add animation module to NgModule

--- letitbee/src/app/app.module.ts
+++ letitbee/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
